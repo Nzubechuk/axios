edaafec30b710201fd3772893e849925a1ed06ec
Nzubechuk <ugwunzubechukwu6@gmail.com>
2025-03-05 01:38:37 -0800

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,30 +1,28 @@
-import React, { useEffect, useState, } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { WeatherContainer, Temp, City } from './WeatherStyle'
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=enugu&units=imperial&appid=bea77714696f983525fb882a721ae7eb'
-
 const Weather = () => {
+    const [city, setCity] = useState('enugu')
     const [data, setData] = useState(null)
 
     useEffect(() => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=bea77714696f983525fb882a721ae7eb`
         axios.get(url).then((response) => {
             setData(response.data)
         }).catch((error) => {
             console.log(error)
         })
-    },[])
-
-    console.log(data)
+    }, [city])
 
     if(!data) return null;
 
-  return (
-    <WeatherContainer>
-        <Temp>{data.main.temp.toFixed(0)}&#176;</Temp>
-        <City>ENUGU, NGR</City>
-    </WeatherContainer>
-  )
+    return (
+        <WeatherContainer>
+            <Temp>{data.main.temp.toFixed(0)}&#176;</Temp>
+            <City>{data.name}, {data.sys.country}</City>
+        </WeatherContainer>
+    )
 }
 
 export default Weather
